11a96b02881656c397f92c21d9c8854a0ed57a57
DaviMF29 <migueldavi313@gmail.com>
2024-03-22 19:54:31 -0300

    waitress

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from flask_jwt_extended import JWTManager
 from routes.user_routes import users_app
 import os
 from pymongo import MongoClient
+from waitress import serve
 
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
 jwt = JWTManager(app)
 
+
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client.get_database(os.getenv("MONGODB_DBNAME"))
-# Registrar as rotas do Blueprint dos posts
+
 app.register_blueprint(users_app)
 
 
-
 if __name__ == "__main__":
-    app.run()
+    serve(app, host="0.0.0.0", port=5000)
