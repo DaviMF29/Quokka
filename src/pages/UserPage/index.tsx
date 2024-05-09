d43439bda5242ddf3cd9869eeb0540c1c5af4298
EliasMedeirosDev <eliasmedeiros898@gmail.com>
2024-05-09 17:38:32 -0300

    ajustado o carregamento do componente

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -28,7 +28,7 @@ export function UserPage({userId}: {userId: string}) {
     const user = useAuth()
     const {userName} = useParams()
     const [pageOwner, setPageOwner] = useState<UserInfo | null>(null)
-    
+    const [pageLoaded, setPageLoaded] = useState(false)
     
     async function getUserByUsername() {
         const response = await api.get(`/api/users/${userName}`)
@@ -40,7 +40,8 @@ export function UserPage({userId}: {userId: string}) {
         useEffect(() => {
             getUserByUsername()
             user.getUserInfo(user.access_token ?? '')
-        }, [pageOwner])
+            setPageLoaded(true)
+        }, [pageLoaded])
 
 
         
@@ -49,8 +50,9 @@ export function UserPage({userId}: {userId: string}) {
         async function handleFollow() {
             
             if(user.access_token && user.userId && pageOwner?._id){
-               user.followUser(user.access_token,user.userId,pageOwner?._id) 
+               await user.followUser(user.access_token,user.userId,pageOwner?._id) 
                getUserByUsername()
+               setPageLoaded(false)
                
             }
             
