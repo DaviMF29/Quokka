c90e3f7f480e6533c83a3dbec5bcb8daeb3ef773
DaviMF29 <migueldavi313@gmail.com>
2024-05-23 11:15:03 -0300

    Refactor create_post_route to include postId in required_fields

--- routes/comment_routes.py
+++ routes/comment_routes.py
@@ -17,17 +17,17 @@ def create_post_route():
     if data is None or data == {}:
         return jsonify({"message": "Empty body"}), 400
 
-    required_fields = ["username", "userId", "text", "createdAt"]
+    required_fields = ["username", "userId", "text", "createdAt", "postId"]
     if not all(field in data for field in required_fields):
         return jsonify({"message": "Missing required fields"}), 400
 
     username = data["username"]
     userId = data["userId"]
-    text = data["text"] 
+    text = data["text"]
     createdAt = data["createdAt"]
     postId = data["postId"]
 
-    post_id = create_comment_controller(postId,userId, username, text, createdAt)
+    post_id = create_comment_controller(postId, userId, username, text, createdAt)
     return jsonify({"id": post_id, "message": f"Post '{text}' created"}), 201
 
 @comment_app.route("/api/comments/<comment_id>", methods=["DELETE"])
