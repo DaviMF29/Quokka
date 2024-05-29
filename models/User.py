631b799e1d12a5421d5bbe05c3e172ffaaa9d760
DaviMF29 <migueldavi313@gmail.com>
2024-05-29 10:44:55 -0300

    refactor: Update User model methods to use users_collection consistently

--- models/User.py
+++ models/User.py
@@ -5,13 +5,13 @@ import os
 client = MongoClient(os.getenv("MONGODB_URI"))
 db_name = "redesocial"
 db = client[db_name]
-users_collection = db.users
 
 
 class User:
 
     @staticmethod
     def create_user_model(username,email,image, hashed_password_base64):
+        users_collection = db.users
         new_user = {
             "username": username,
             "email": email,
@@ -28,6 +28,7 @@ class User:
 
     @staticmethod
     def get_all_users_model():
+        users_collection = db.users
         users = users_collection.find()
         serialized_users = []
         for user in users:
@@ -37,18 +38,21 @@ class User:
 
     @staticmethod
     def get_all_posts_from_user(user_id):
+        users_collection = db.users
         user = users_collection.find_one({"_id": ObjectId(user_id)})
         posts = user.get("posts", [])
         return posts
 
     @staticmethod
     def get_posts_liked_by_user_model(user_id):
+        users_collection = db.users
         user = users_collection.find_one({"_id":ObjectId(user_id)})
         liked_posts = user.get("liked_posts",[])
         return liked_posts
     
     @staticmethod
     def get_user_by_username_model(username):
+        users_collection = db.users
         user = users_collection.find_one({"username": username})
         if user:
             user["_id"] = str(user["_id"])
@@ -57,11 +61,13 @@ class User:
     
     @staticmethod
     def get_user_by_email_model(email):
+        users_collection = db.users
         user = users_collection.find_one({"email": email})
         return user
     
     @staticmethod
     def get_user_by_id_model(id):
+        users_collection = db.users
         user = users_collection.find_one({"_id": ObjectId(id)})
         if user:
             user["_id"] = str(user["_id"])
@@ -70,16 +76,19 @@ class User:
     
     @staticmethod
     def update_user(user_id, updated_fields):
+        users_collection = db.users
         result = users_collection.update_one({"_id": ObjectId(user_id)}, {"$set": updated_fields})
         return result
     
     @staticmethod
     def update_many_user(query, updated_fields):
+        users_collection = db.users
         result = users_collection.update_many(query, {"$pull": updated_fields})
         return result
 
     @staticmethod
     def get_followers_model(user_id):
+        users_collection = db.users
         user = users_collection.find_one({"_id": ObjectId(user_id)})
         if user:
             followers = user.get("followers", [])
@@ -89,6 +98,7 @@ class User:
     
     @staticmethod
     def get_following_model(user_id):
+        users_collection = db.users
         user = users_collection.find_one({"_id": ObjectId(user_id)})
         if user:
             following = user.get("following", [])
@@ -98,6 +108,7 @@ class User:
 
     @staticmethod
     def get_favorite_posts_model(user_id):
+        users_collection = db.users
         user = users_collection.find_one({"_id": ObjectId(user_id)})
         if user:
             favorites = user.get("favorites", [])
@@ -107,21 +118,23 @@ class User:
         
     @staticmethod
     def delete_account_model(user_id):
+        users_collection = db.users
         result = users_collection.find_one_and_delete({"_id": ObjectId(user_id)})
         return result
     
     def add_new_field_to_all_users(new_field_name):
+        users_collection = db.users
         result = users_collection.update_many({}, {"$set": {new_field_name: []}})
         return result
 
     
     def update_user_image_model(user_id, image_url):
+        users_collection = db.users
         result = users_collection.update_one(
             {'_id': ObjectId(user_id)},
             {'$set': {'image': image_url}}
         )
 
-       
         if result.modified_count == 0:
             raise Exception(f"Falha ao atualizar a imagem do usuário com id {user_id}.")
 
